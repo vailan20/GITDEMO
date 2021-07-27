20cf9747f055caf170caf84200a823249dc2e38f
Aakanksha-Kapure <55625231+Aakanksha-Kapure@users.noreply.github.com>
2021-07-27 21:37:46 +0530

    Update Program.cs

--- prjFirstApplication/Program.cs
+++ prjFirstApplication/Program.cs
@@ -7,6 +7,7 @@ namespace prjFirstApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("GIT Assignment 1");
         }
     }
 }
