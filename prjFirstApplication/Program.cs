adcd1f51157a09f44c269cf774bd3d91be1a111d
Vailantina Fernandes <62817540+vailan20@users.noreply.github.com>
2021-07-27 21:24:47 +0530

    Add method added in Program.cs file

--- prjFirstApplication/Program.cs
+++ prjFirstApplication/Program.cs
@@ -4,6 +4,9 @@ namespace prjFirstApplication
 {
     class Program
     {
+        void Add(){
+            int c = 34 +34;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
